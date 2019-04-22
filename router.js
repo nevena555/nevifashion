b5cc8b887afc036ddb4218dd786d6f04de8db855
nevena555 <48865642+nevena555@users.noreply.github.com>
2019-04-23 01:57:08 +0300

    Add files via upload

--- router.js
+++ router.js
@@ -1,6 +1,23 @@
 const express = require('express');
 var router = express.Router();
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var path = require('path');
+
+
+var loginController = require('./controllers/login-controller');
+
+//Static files
+router.use(session({
+  secret: 'awdawd',
+  resave: false,
+  saveUninitialized: true
+}));
+router.use(bodyParser.urlencoded({extended:true}));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+var sess;
 
 // route to handle entry point
 router.get('/', function(req, res){
@@ -10,7 +27,21 @@ router.get('/', function(req, res){
 router.get('/gallery', function(req, res){
   res.render('pages/gallery');
 });
-// route to handle login form
+router.get('/login', function(req, res){
+  res.render('pages/login',{
+    alert_danger: "",
+    error: ""
+  });
+});
+router.post('/controllers/login-controller', loginController.authenticate);
+
+router.get('/home', function(req, res){
+  sess = req.session;
+  res.render('pages/home',{
+    user: sess.username
+  });
+});
+// route to handle contact form
 router.get('/contact', function(req, res){
   res.render('pages/contact');
 });
@@ -22,6 +53,15 @@ router.get('/about', function(req, res){
     Message: 'message'
   });
 });
-
+router.get('/logout',function(req,res){
+  req.session.destroy(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+      console.log(req.session);
+    }
+  });
+});
 
 module.exports = router;
